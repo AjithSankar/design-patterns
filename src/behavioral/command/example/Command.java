01f69f8a76852ba69d304ccf1f12ba333978451c
ajithkumar.sankar <mailajithkumars@gmail.com>
2025-07-19 09:16:37 +0530

    Command Pattern - Text editor implementation

---
+++ src/behavioral/command/example/Command.java
@@ -0,0 +1,21 @@
+package behavioral.command.example;
+
+public abstract class Command {
+
+    protected Editor editor;
+    protected String backup;
+
+    public Command(Editor editor) {
+        this.editor = editor;
+    }
+
+    public void backup() {
+        backup = editor.textField.getText();
+    }
+
+    public void undo() {
+        editor.textField.setText(backup);
+    }
+
+    public abstract boolean execute();
+}
---
+++ src/behavioral/command/example/GUIEditor.java
@@ -0,0 +1,8 @@
+package behavioral.command.example;
+
+public class GUIEditor {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
